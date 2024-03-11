0bf96989cceb68df12c149e08206d8768dbf894a
Jesse Sheehan <jesse@sheehan.nz>
2024-03-11 19:01:25 +1300

    Assets

---
+++ Assets.mjs
@@ -0,0 +1,12 @@
+export const images = {
+    "player_fall": "assets/player_fall.png",
+    "player_idle": "assets/player_idle.png",
+    "player_jump": "assets/player_jump.png",
+    "player_run": "assets/player_run.png",
+};
+
+export const sounds = {
+};
+
+export const assets = { images };
+
